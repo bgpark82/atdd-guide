a9c502e637a3f16df4c8190b0dc708a93fc9ad5c
peter-alethio <peter@alethio.io>
2021-07-27 17:48:16 +0900

    feat: 프로젝트 시작

---
+++ src/main/java/com/bgpark/atdd/AtddGuideApplication.java
@@ -0,0 +1,13 @@
+package com.bgpark.atdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtddGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtddGuideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bgpark/atdd/AtddGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgpark.atdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtddGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
