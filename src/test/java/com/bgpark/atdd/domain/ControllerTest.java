f16e25bd05fe6cb9ab7fad34aa3af9ea2a2d3def
peter-alethio <peter@alethio.io>
2021-07-28 11:03:16 +0900

    refactoring: Controller 테스트 분리

---
+++ src/test/java/com/bgpark/atdd/domain/ControllerTest.java
@@ -0,0 +1,19 @@
+package com.bgpark.atdd.domain;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class ControllerTest {
+
+    @Autowired
+    protected MockMvc mvc;
+
+    @Autowired
+    protected ObjectMapper mapper;
+}
--- src/test/java/com/bgpark/atdd/domain/user/controller/UserControllerTest.java
+++ src/test/java/com/bgpark/atdd/domain/user/controller/UserControllerTest.java
@@ -3,17 +3,11 @@ package com.bgpark.atdd.domain.user.controller;
 import com.bgpark.atdd.domain.ControllerTest;
 import com.bgpark.atdd.domain.user.dto.UserSaveRequest;
 import com.bgpark.atdd.domain.user.service.UserService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
 import static com.bgpark.atdd.domain.user.step.UserSteps.createSaveRequest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,14 +16,10 @@ import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest
-public class UserControllerTest {
+public class UserControllerTest extends ControllerTest{
 
-    @Autowired private MockMvc mvc;
-    @Autowired private ObjectMapper mapper;
-
-    @MockBean private UserService userService;
+    @MockBean
+    private UserService userService;
 
     @Test
     public void save_사용자를_저장한다() throws Exception {
