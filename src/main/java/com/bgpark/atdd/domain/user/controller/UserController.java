6807d0d1d94b23f9bd3071b13aceed268fe73c01
peter-alethio <peter@alethio.io>
2021-07-28 10:29:29 +0900

    refactoring: 사용자 저장 Controller에 Service 추가

--- src/main/java/com/bgpark/atdd/domain/user/controller/UserController.java
+++ src/main/java/com/bgpark/atdd/domain/user/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.bgpark.atdd.domain.user.controller;
 
 import com.bgpark.atdd.domain.user.dto.UserSaveRequest;
+import com.bgpark.atdd.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,10 +12,14 @@ import javax.validation.Valid;
 import java.net.URI;
 
 @RestController
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
 
     @PostMapping("/users")
     public ResponseEntity save(@Valid @RequestBody UserSaveRequest request) {
+        userService.save(request);
         return ResponseEntity.created(URI.create("/users")).build();
     }
 }
