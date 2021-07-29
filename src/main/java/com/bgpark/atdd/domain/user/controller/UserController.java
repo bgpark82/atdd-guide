a4be9c8cab2bd24cca209c08847ff58473763e00
peter-alethio <peter@alethio.io>
2021-07-29 10:46:02 +0900

    feat: 사용자 조회 Controller 구현

--- src/main/java/com/bgpark/atdd/domain/user/controller/UserController.java
+++ src/main/java/com/bgpark/atdd/domain/user/controller/UserController.java
@@ -1,12 +1,11 @@
 package com.bgpark.atdd.domain.user.controller;
 
+import com.bgpark.atdd.domain.user.dto.UserFindResponse;
 import com.bgpark.atdd.domain.user.dto.UserSaveRequest;
 import com.bgpark.atdd.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
@@ -21,5 +20,10 @@ public class UserController {
     public ResponseEntity save(@Valid @RequestBody UserSaveRequest request) {
         userService.save(request);
         return ResponseEntity.created(URI.create("/users")).build();
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity find(@PathVariable Long id) {
+        return ResponseEntity.ok(new UserFindResponse(1L, "박병길", "123456"));
     }
 }
