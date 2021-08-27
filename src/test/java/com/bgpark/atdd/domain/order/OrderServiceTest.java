ae0700d145a870a9442805c333855f62afdb3668
peter-alethio <peter@alethio.io>
2021-08-27 10:57:43 +0900

    refactor: InMemoryRepository 적용

--- src/test/java/com/bgpark/atdd/domain/order/OrderServiceTest.java
+++ src/test/java/com/bgpark/atdd/domain/order/OrderServiceTest.java
@@ -3,54 +3,37 @@ package com.bgpark.atdd.domain.order;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Optional;
-
-import static com.bgpark.atdd.domain.order.OrderFixture.*;
+import static com.bgpark.atdd.domain.order.OrderFixture.createProduct;
+import static com.bgpark.atdd.domain.order.OrderFixture.createSaveOrderRequest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
 
-@ExtendWith(MockitoExtension.class)
 public class OrderServiceTest {
 
-    @Mock
-    OrderRepository orderRepository;
+    String 상품_이름 = "햄버거";
+    int 상품_수량 = 10;
+    int 상품_가격 = 1000;
+    int 주문_가격 = 10000;
 
-    @Mock
-    ProductRepository productRepository;
+    OrderRepository orderRepository = new InMemoryOrderRepository();
+    ProductRepository productRepository = new InMemoryProductRepository();
 
     OrderService orderService;
 
     @BeforeEach
     void setUp() {
         orderService = new OrderService(orderRepository, productRepository);
+        Product product = createProduct(상품_이름, 상품_가격);
+        productRepository.save(product);
     }
 
     @DisplayName("create, 주문을 생성한다")
     @Test
     void create_주문_생성() {
-
         // given
-        String 상품_이름 = "햄버거";
-        int 상품_수량 = 10;
-        int 상품_가격 = 1000;
-        int 주문_가격 = 10000;
-
         Orders request = createSaveOrderRequest(상품_수량, 주문_가격);
-
-        Product product = createProduct(상품_이름, 상품_가격);
-        given(productRepository.findById(any())).willReturn(Optional.of(product));
-
-        Product product1 = createProduct(상품_이름, 상품_가격);
-        OrderItem orderItem = createOrderItem(상품_수량, product1);
-        Orders orders = createOrder(주문_가격, orderItem);
-        given(orderRepository.save(any())).willReturn(orders);
 
         // when
         Orders savedOrder = orderService.create(request);
@@ -67,15 +50,8 @@ public class OrderServiceTest {
     @DisplayName("create, 주문 생성 시 주문 가격과 상품 가격 * 상품 수량이 다른 경우 에러를 발생한다")
     @Test
     void create_다른_가격() {
-
         // given
-        // request 생성
         Orders request = createSaveOrderRequest(-1, 10000);
-
-        // product 저장
-        Product product = createProduct("햄버거", 1000);
-
-        given(productRepository.findById(any())).willReturn(Optional.of(product));
 
         // when
         assertThatExceptionOfType(IllegalStateException.class)
